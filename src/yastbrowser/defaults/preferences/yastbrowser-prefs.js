7a05db31cc61bc4cfcba7b18c19a28e3de2424e3
J. Daniel Schmidt <jdsn@suse.de>
2006-11-17 17:10:33 +0000

    - yastbrowser update, support for mozilla-xulrunner181 (#219354) - removed dependencies to rug and zmd for openSUSE (#220807) - hard dependencies to rug and zmd for SLE (#220807) - check for wrong proxy settings (#208651) - removed SuSEregister.pango call (FATE #300947)
    svn path=/trunk/registration/; revision=34449

--- src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
+++ src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
@@ -1,5 +1,8 @@
 pref("toolkit.defaultChromeURI", "chrome://yastbrowser/content/yastbrowser.xul");
-pref("general.useragent.extra.yastbrowser", "YaST-Browser/0.1");
+pref("general.useragent.extra.yastbrowser", "YaST-Browser/0.2");
 pref("security.warn_entering_secure", false);
 pref("dom.allow_scripts_to_close_windows", true);
 pref("network.proxy.type", 5);
+//pref("accessibility.typeaheadfind", false);
+//pref("accessibility.typeaheadfind.linksonly", false);
+//pref("accessibility.typeaheadfind.casesensitive", false);
