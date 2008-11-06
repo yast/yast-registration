6a21f6f4a13660c4ef68c8ee7e4be9510eb42ca2
J. Daniel Schmidt <jdsn@suse.de>
2008-11-06 14:25:46 +0000

    split off branding packages (bnc#436644) fix error code handling of SuseRegister updated yastbrowser for xulrunner191
    svn path=/trunk/registration/; revision=52996

--- src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
+++ src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
@@ -1,8 +1,10 @@
 pref("toolkit.defaultChromeURI", "chrome://yastbrowser/content/yastbrowser.xul");
-pref("general.useragent.extra.yastbrowser", "YaST-Browser/0.2");
+pref("general.useragent.extra.yastbrowser", "YaST-Browser/0.3");
 pref("security.warn_entering_secure", false);
 pref("dom.allow_scripts_to_close_windows", true);
 pref("network.proxy.type", 5);
-//pref("accessibility.typeaheadfind", false);
-//pref("accessibility.typeaheadfind.linksonly", false);
-//pref("accessibility.typeaheadfind.casesensitive", false);
+pref("accessibility.typeaheadfind.timeout", 5000);
+pref("accessibility.typeaheadfind.flashBar", 1);
+pref("accessibility.typeaheadfind", false);
+pref("accessibility.typeaheadfind.linksonly", false);
+pref("accessibility.typeaheadfind.casesensitive", 0);
