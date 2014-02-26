7fcdc35a363a24614f36566edf7c609042c71a38
Ladislav Slezák <lslezak@suse.cz>
2014-02-26 17:31:10 +0100

    move language function to a separate file

---
+++ src/lib/registration/helpers.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2014 Novell, Inc. All Rights Reserved.
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact Novell, Inc.
+#
+# To contact Novell about this file by physical or electronic mail, you may find
+# current contact information at www.novell.com.
+# ------------------------------------------------------------------------------
+#
+#
+
+require "yast"
+
+module Registration
+  Yast.import "Language"
+
+  class Helpers
+    include Yast
+    include Yast::Logger
+
+    def self.language
+      lang = Language.language
+      log.info "Current language: #{lang}"
+
+      # remove the encoding (e.g. ".UTF-8")
+      lang.sub!(/\..*$/, "")
+      # replace lang/country separator "_" -> "-"
+      # see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+      lang.tr!("_", "-")
+
+      log.info "Language for HTTP requests set to #{lang.inspect}"
+      lang
+    end
+
+  end
+end
