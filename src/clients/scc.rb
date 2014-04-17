f0dd8fb5451c068b44a805af935bd66887f078f1
Ladislav Slezák <lslezak@suse.cz>
2014-04-17 11:53:33 +0200

    check the libzypp lock at start (bnc#873882)
    do not continue if libzypp is already locked by another application

--- src/clients/scc.rb
+++ src/clients/scc.rb
@@ -45,7 +45,7 @@ module Yast
         CommandLine.Run(cmdline_description)
       else
         Wizard.CreateDialog
-        ::Registration::SwMgmt.init
+        return false unless ::Registration::SwMgmt.init
 
         begin
           return WFM.call("inst_scc")
