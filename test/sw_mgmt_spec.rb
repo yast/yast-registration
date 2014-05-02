1646f4d305391a9f63e709ffcb12c25b078917b9
Ladislav Slezák <lslezak@suse.cz>
2014-05-02 18:13:13 +0200

    activate the repository setup as set in the SCC response (bnc#875839)

--- test/sw_mgmt_spec.rb
+++ test/sw_mgmt_spec.rb
@@ -8,6 +8,48 @@ require "suse/connect"
 describe "Registration::SwMgmt" do
   let(:yast_pkg) { double("Yast::Pkg") }
 
+  let(:service_name) { "SLES" }
+  let(:repos) do
+    {
+      # installation repository, not from registration
+      0 => {
+        "alias"=>"SLES12", "autorefresh"=>false,
+        "base_urls"=>["cd:///"],
+        "enabled"=>true, "is_update_repo"=>false, "keeppackages"=>false,
+        "mirror_list"=>"", "name"=>"SLES12", "priority"=>99, "product_dir"=>"",
+        "service"=>"", "type"=>"SUSE",
+        "url"=>"cd:///"
+      },
+      # pool repo from service
+      1 => {
+        "alias"=>"SLES:SLES12-Pool", "autorefresh"=>true,
+        "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12-POOL"],
+        "enabled"=>true, "is_update_repo"=>false, "keeppackages"=>false,
+        "mirror_list"=>"", "name"=>"SLES12-Pool", "priority"=>99, "product_dir"=>"",
+        "service"=>service_name, "type"=>"YUM",
+        "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12-POOL"
+      },
+      # update repo from service
+      2 => {
+        "alias"=>"SLES:SLES12-Updates", "autorefresh"=>true,
+        "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"],
+        "enabled"=>true, "is_update_repo"=>true, "keeppackages"=>false,
+        "mirror_list"=>"", "name"=>"SLES12-Updates", "priority"=>99, "product_dir"=> "",
+        "service"=>service_name, "type"=>"YUM",
+        "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"
+      },
+      # update repo from a different service
+      3 => {
+        "alias"=>"Another:SLES12-Updates", "autorefresh"=>true,
+        "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"],
+        "enabled"=>true, "is_update_repo"=>true, "keeppackages"=>false,
+        "mirror_list"=>"", "name"=>"SLES12-Updates", "priority"=>99, "product_dir"=> "",
+        "service"=> "Another", "type"=>"YUM",
+        "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"
+      }
+    }
+  end
+
   before do
     stub_yast_require
     require "registration/sw_mgmt"
@@ -17,47 +59,6 @@ describe "Registration::SwMgmt" do
 
   describe ".service_repos" do
     let(:services) { double }
-    let(:service_name) { "SLES" }
-    let(:repos) do
-      {
-        # installation repository, not from registration
-        0 => {
-          "alias"=>"SLES12", "autorefresh"=>false,
-          "base_urls"=>["cd:///"],
-          "enabled"=>true, "is_update_repo"=>false, "keeppackages"=>false,
-          "mirror_list"=>"", "name"=>"SLES12", "priority"=>99, "product_dir"=>"",
-          "service"=>"", "type"=>"SUSE",
-          "url"=>"cd:///"
-        },
-        # pool repo from service
-        1 => {
-          "alias"=>"SLES:SLES12-Pool", "autorefresh"=>true,
-          "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12-POOL"],
-          "enabled"=>true, "is_update_repo"=>false, "keeppackages"=>false,
-          "mirror_list"=>"", "name"=>"SLES12-Pool", "priority"=>99, "product_dir"=>"",
-          "service"=>service_name, "type"=>"YUM",
-          "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12-POOL"
-        },
-        # update repo from service
-        2 => {
-          "alias"=>"SLES:SLES12-Updates", "autorefresh"=>true,
-          "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"],
-          "enabled"=>true, "is_update_repo"=>true, "keeppackages"=>false,
-          "mirror_list"=>"", "name"=>"SLES12-Updates", "priority"=>99, "product_dir"=> "",
-          "service"=>service_name, "type"=>"YUM",
-          "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"
-        },
-        # update repo from a different service
-        3 => {
-          "alias"=>"Another:SLES12-Updates", "autorefresh"=>true,
-          "base_urls"=>["https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"],
-          "enabled"=>true, "is_update_repo"=>true, "keeppackages"=>false,
-          "mirror_list"=>"", "name"=>"SLES12-Updates", "priority"=>99, "product_dir"=> "",
-          "service"=> "Another", "type"=>"YUM",
-          "url"=>"https://nu.novell.com/suse/x86_64/update/SLE-SERVER/12"
-        }
-      }
-    end
 
     before do
       service = double
@@ -93,25 +94,36 @@ describe "Registration::SwMgmt" do
     let(:service_url) { "https://example.com/foo/bar?credentials=TEST_credentials" }
     let(:credentials) { SUSE::Connect::Credentials.new("user", "password", "file") }
     let(:product_services) do
-      SUSE::Connect::Service.new({"test" => service_url}, [], [])
+      SUSE::Connect::Service.new({service_name => service_url}, ["SLES:SLES12-Pool"],
+        ["SLES:SLES12-Pool"])
     end
 
     before do
       expect(yast_pkg).to receive(:SourceSaveAll).and_return(true).twice
-      expect(yast_pkg).to receive(:ServiceRefresh).with("test").and_return(true)
-      expect(yast_pkg).to receive(:ServiceSave).with("test").and_return(true)
+      expect(yast_pkg).to receive(:ServiceRefresh).with(service_name).and_return(true)
+      expect(yast_pkg).to receive(:ServiceSave).with(service_name).and_return(true)
       SUSE::Connect::Credentials.any_instance.should_receive(:write)
+
+      # 1 -> "SLES:SLES12-Pool"
+      expect(yast_pkg).to receive(:SourceSetEnabled).with(1, true).and_return(true)
+      expect(yast_pkg).to receive(:SourceSetAutorefresh).with(1, false).and_return(true)
+
+      allow(yast_pkg).to receive(:SourceGetCurrent).with(false).and_return(repos.keys)
+      repos.each do |id, repo|
+        allow(yast_pkg).to receive(:SourceGeneralData).with(id).and_return(repo)
+      end
     end
 
     it "it creates a new service if the service does not exist yet" do
       expect(yast_pkg).to receive(:ServiceAliases).and_return([])
-      expect(yast_pkg).to receive(:ServiceAdd).with("test", service_url).and_return(true)
+      expect(yast_pkg).to receive(:ServiceAdd).with(service_name, service_url).and_return(true)
       expect { Registration::SwMgmt.add_services([product_services], credentials) }.to_not raise_error
     end
 
     it "updates the existing service if the service already exists" do
-      expect(yast_pkg).to receive(:ServiceAliases).and_return(["test"])
-      expect(yast_pkg).to receive(:ServiceSet).with("test", hash_including("url" => service_url)).and_return(true)
+      expect(yast_pkg).to receive(:ServiceAliases).and_return([service_name])
+      expect(yast_pkg).to receive(:ServiceSet).with(
+        service_name, hash_including("url" => service_url)).and_return(true)
       expect { Registration::SwMgmt.add_services([product_services], credentials) }.to_not raise_error
     end
   end
