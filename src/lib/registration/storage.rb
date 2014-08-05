1176de1fb708901c85ec5ab853fb9a0d5d23f802
Ladislav Slezák <lslezak@suse.cz>
2014-08-05 18:08:15 +0200

    refactor export/import (decrease complexity)

--- src/lib/registration/storage.rb
+++ src/lib/registration/storage.rb
@@ -99,39 +99,19 @@ module Registration
       end
 
       def export
-        ret = { "do_registration" => @do_registration }
+        ret = { "do_registration" => do_registration }
+        return ret unless do_registration
 
-        if @do_registration
-          ret.merge!(
-            {
-              "reg_server" => @reg_server,
-              "slp_discovery" => @slp_discovery,
-              "reg_server_cert" => @reg_server_cert,
-              "email" => @email,
-              "reg_code" => @reg_code,
-              "install_updates" => @install_updates
-            }
-          )
+        ret.merge!(
+          "reg_server" => reg_server,
+          "slp_discovery" => slp_discovery,
+          "email" => email,
+          "reg_code" => reg_code,
+          "install_updates" => install_updates
+        )
 
-          # handle nil release_type at export
-          addons_export = @addons.map do |a|
-            addon_export = a.dup
-            addon_export["release_type"] = "nil" if addon_export["release_type"].nil?
-            addon_export
-          end
-
-          ret["addons"] = addons_export
-
-          if reg_server_cert_fingerprint_type &&
-            reg_server_cert_fingerprint_type.upcase == "SHA1" ||
-            reg_server_cert_fingerprint_type.upcase == "SHA256"
-
-            ret.merge!(
-              "reg_server_cert_fingerprint_type" => reg_server_cert_fingerprint_type,
-              "reg_server_cert_fingerprint" => reg_server_cert_fingerprint,
-            )
-          end
-        end
+        ret["addons"] = export_addons
+        ret.merge!(export_ssl_config)
 
         ret
       end
@@ -146,17 +126,46 @@ module Registration
         @email = settings["email"] || ""
         @reg_code = settings["reg_code"] || ""
         @install_updates = settings.fetch("install_updates", false)
-
-        # handle "nil" release_type
-        @addons = (settings["addons"] || []).map do |a|
-          import_addon = a.dup
-          import_addon["release_type"] = nil if a["release_type"] == "nil"
-          import_addon
-        end
-
+        @addons = import_addons(settings)
         @reg_server_cert_fingerprint_type = settings["reg_server_cert_fingerprint_type"] || ""
         @reg_server_cert_fingerprint = settings["reg_server_cert_fingerprint"] || ""
       end
+
+
+      private
+
+      def import_addons(settings)
+        (settings["addons"] || []).map do |a|
+          import_addon = a.dup
+          # handle "nil" release_type
+          import_addon["release_type"] = nil if a["release_type"] == "nil"
+          import_addon
+        end
+      end
+
+      def export_addons
+        addons.map do |a|
+          addon_export = a.dup
+          # handle nil release_type at export
+          addon_export["release_type"] = "nil" if addon_export["release_type"].nil?
+          addon_export
+        end
+      end
+
+      def export_ssl_config
+        ret = { "reg_server_cert" => reg_server_cert }
+
+        if reg_server_cert_fingerprint_type &&
+            reg_server_cert_fingerprint_type.upcase == "SHA1" ||
+            reg_server_cert_fingerprint_type.upcase == "SHA256"
+
+          ret["reg_server_cert_fingerprint_type"] = reg_server_cert_fingerprint_type
+          ret["reg_server_cert_fingerprint"] = reg_server_cert_fingerprint
+        end
+
+        ret
+      end
+
     end
   end
 end
