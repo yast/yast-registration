4f2687cec738bb975c6445bfefaead70abc473e6
Ladislav Slezák <lslezak@suse.cz>
2014-08-20 16:29:08 +0200

    small refactoring in discover_registration_services.rb

--- src/clients/discover_registration_services.rb
+++ src/clients/discover_registration_services.rb
@@ -26,59 +26,71 @@ module Yast
 
     private
 
-    def select_registration_service
-      UI.OpenDialog(
-        Opt(:decorated),
-        MarginBox(2, 0.5,
-          VBox(
-            # popup heading (in bold)
-            Heading(_('Local Registration Servers')),
-            VSpacing(0.5),
-            Label(_("Select a detected registration server from the list\n" +
-                  "or the default SUSE registration server.")),
-            VSpacing(0.5),
-            RadioButtonGroup(
-              Id(:services),
-              Left(
-                HVSquash(
-                  VBox(*services_radio_buttons)
-                )
+    def button_box
+      ButtonBox(
+        PushButton(Id(:ok), Opt(:default), Label.OKButton),
+        PushButton(Id(:cancel), Label.CancelButton)
+      )
+    end
+
+    def dialog_content
+      MarginBox(2, 0.5,
+        VBox(
+          # popup heading (in bold)
+          Heading(_('Local Registration Servers')),
+          VSpacing(0.5),
+          Label(_("Select a detected registration server from the list\n" +
+                "or the default SUSE registration server.")),
+          VSpacing(0.5),
+          RadioButtonGroup(
+            Id(:services),
+            Left(
+              HVSquash(
+                VBox(*services_radio_buttons)
               )
-            ),
-            VSpacing(Opt(:vstretch), 1),
-            ButtonBox(
-              PushButton(Id(:ok), Opt(:default), Label.OKButton),
-              PushButton(Id(:cancel), Label.CancelButton)
             )
-          )
+          ),
+          VSpacing(Opt(:vstretch), 1),
+          button_box
         )
       )
+    end
+
+    def handle_dialog
+      loop do
+        case UI.UserInput
+        when :ok
+          selected = UI.QueryWidget(Id(:services), :CurrentButton)
+          if !selected
+            # error popup
+            Report.Error(_("No registration server selected."))
+            next
+          end
+
+          break if selected == "scc"
+
+          return service_url(selected)
+        when :cancel
+          return :cancel
+        end
+      end
+    end
+
+    def service_url(selected)
+      selected_service = services[selected.to_i]
+      log.info "Selected service #{selected_service.inspect}"
+
+      url = ::Registration::UrlHelpers.service_url(selected_service.slp_url)
+      log.info "Selected service URL: #{url}"
+
+      return url
+    end
+
+    def select_registration_service
+      UI.OpenDialog(Opt(:decorated), dialog_content)
 
       begin
-        loop do
-          dialog_result = UI.UserInput
-          case dialog_result
-          when :ok
-            selected = UI.QueryWidget(Id(:services), :CurrentButton)
-            if !selected
-              # error popup
-              Report.Error(_("No registration server selected."))
-              next
-            end
-
-            break if selected == "scc"
-
-            selected_service = services[selected.to_i]
-            log.info "Selected service #{selected_service.inspect}"
-
-            url = ::Registration::UrlHelpers.service_url(selected_service.slp_url)
-            log.info "Selected service URL: #{url}"
-
-            return url
-          when :cancel
-            return :cancel
-          end
-        end
+        return handle_dialog
       ensure
         UI.CloseDialog
       end
@@ -96,9 +108,9 @@ module Yast
       end
 
       widgets.unshift(Left(RadioButton(Id("scc"),
-          # %s is the default SCC URL
-          _("SUSE Customer Center (%s)") % SUSE::Connect::Client::DEFAULT_URL,
-          true)))
+            # %s is the default SCC URL
+            _("SUSE Customer Center (%s)") % SUSE::Connect::Client::DEFAULT_URL,
+            true)))
     end
 
   end unless defined?(DiscoverRegistrationServicesClient)
