761288b085f5b05342a8d979170c3bbaeed4c4fe
Ladislav Slezák <lslezak@suse.cz>
2015-04-22 08:42:43 +0200

    improve media_addon_workflow_spec.rb test
    share the mocks to decrease the size of the tests

--- test/media_addon_workflow_spec.rb
+++ test/media_addon_workflow_spec.rb
@@ -11,19 +11,20 @@ describe "Registration::UI::MediaAddonWorkflow" do
     let(:remote_addons) { YAML.load_file(fixtures_file("available_addons.yml")) }
 
     before do
-      expect(Registration::UrlHelpers).to receive(:registration_url)
+      allow(Registration::UrlHelpers).to receive(:registration_url)
+      allow(Registration::SwMgmt).to receive(:init).and_return(true)
+      allow(Yast::Pkg).to receive(:SourceLoad)
+      allow(Yast::Pkg).to receive(:SourceGeneralData).with(repo).and_return({})
+      allow(Registration::SwMgmt).to receive(:products_from_repo).with(repo).and_return(products)
+      allow(Registration::SwMgmt).to receive(:select_product_addons).with(products, remote_addons)
+      allow(Registration::Addon).to receive(:selected).twice.and_return([remote_addons.first])
+      allow_any_instance_of(Registration::RegistrationUI).to receive(:get_available_addons)
+        .and_return(remote_addons)
+      allow(Registration::Addon).to receive(:find_all).and_return(remote_addons)
     end
 
     it "registers the addon from media" do
-      expect(Registration::SwMgmt).to receive(:init).and_return(true)
-      expect(Yast::Pkg).to receive(:SourceLoad)
-      expect(Registration::SwMgmt).to receive(:products_from_repo).with(repo).and_return(products)
       expect(Registration::Registration).to receive(:is_registered?).and_return(true)
-      expect_any_instance_of(Registration::RegistrationUI).to receive(:get_available_addons)
-        .and_return(remote_addons)
-      expect(Registration::SwMgmt).to receive(:select_product_addons).with(products, remote_addons)
-      expect(Registration::Addon).to receive(:selected).twice.and_return([remote_addons.first])
-      expect(Registration::Addon).to receive(:find_all).and_return(remote_addons)
       expect_any_instance_of(Registration::RegistrationUI).to receive(:register_addons)
         .and_return(:next)
 
@@ -38,32 +39,18 @@ describe "Registration::UI::MediaAddonWorkflow" do
     end
 
     it "skips registation when the media addon does not provide any product" do
-      expect(Registration::SwMgmt).to receive(:init).and_return(true)
-      expect(Yast::Pkg).to receive(:SourceLoad)
       expect(Registration::SwMgmt).to receive(:products_from_repo).with(repo).and_return([])
-      expect(Yast::Pkg).to receive(:SourceGeneralData).with(repo).and_return({})
-
       expect(Registration::UI::MediaAddonWorkflow.run(repo)).to eq(:finish)
     end
 
     it "registers the base system if it is not registered yet" do
-      expect(Registration::SwMgmt).to receive(:init).and_return(true)
-      expect(Yast::Pkg).to receive(:SourceLoad)
-      expect(Registration::SwMgmt).to receive(:products_from_repo).with(repo).and_return(products)
-
       # the base system registration skipped then registered (to cover more paths)
       expect(Registration::Registration).to receive(:is_registered?).exactly(3).times
         .and_return(false, false, true)
       allow_any_instance_of(Registration::UI::BaseSystemRegistrationDialog).to receive(
         :run).and_return(:skip, :next)
-      expect(Yast::Pkg).to receive(:SourceGeneralData).with(repo).and_return({})
       expect(Yast::Popup).to receive(:YesNo).and_return(false)
 
-      expect_any_instance_of(Registration::RegistrationUI).to receive(:get_available_addons)
-        .and_return(remote_addons)
-      expect(Registration::SwMgmt).to receive(:select_product_addons).with(products, remote_addons)
-      expect(Registration::Addon).to receive(:selected).twice.and_return([remote_addons.first])
-      expect(Registration::Addon).to receive(:find_all).and_return(remote_addons)
       expect_any_instance_of(Registration::RegistrationUI).to receive(:register_addons)
         .and_return(:next)
 
@@ -71,16 +58,11 @@ describe "Registration::UI::MediaAddonWorkflow" do
     end
 
     it "skips addon product registration if base product registration is skipped" do
-      expect(Registration::SwMgmt).to receive(:init).and_return(true)
-      expect(Yast::Pkg).to receive(:SourceLoad)
-      expect(Registration::SwMgmt).to receive(:products_from_repo).with(repo).and_return(products)
-
       # skip base system registration
       expect(Registration::Registration).to receive(:is_registered?).twice.times
         .and_return(false)
       expect_any_instance_of(Registration::UI::BaseSystemRegistrationDialog).to receive(
         :run).and_return(:skip)
-      expect(Yast::Pkg).to receive(:SourceGeneralData).with(repo).and_return({})
       expect(Yast::Popup).to receive(:YesNo).and_return(true)
 
       # no addon is registered
