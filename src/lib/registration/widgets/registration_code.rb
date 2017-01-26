1091a9c06a6fba5e57ead67692edf270450b7a8d
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-01-26 08:28:45 +0000

    Added registration logic to the widget.

--- src/lib/registration/widgets/registration_code.rb
+++ src/lib/registration/widgets/registration_code.rb
@@ -21,15 +21,106 @@
 
 require "yast"
 require "cwm/widget"
+require "uri"
+require "registration/registration_ui"
 
-module Widgets
-  class RegistrationCode < CWM::InputField
-    def initialize
-      textdomain "registration"
-    end
+module Registration
+  module Widgets
+    class RegistrationCode < CWM::InputField
+      VALID_URL_SCHEMES = ["http", "https"].freeze
 
-    def label
-      _("Registration Code or SMT Server URL")
+      attr_accessor :registration_code, :registration, :reg_options
+
+      def initialize
+        textdomain "registration"
+      end
+
+      def label
+        _("Registration Code or SMT Server URL")
+      end
+
+      # Initialize the widget with stored values
+      def init
+        log.info("Already registered") if registered?
+
+        @options = Storage::InstallationOptions.instance
+        self.value = @options.custom_url
+        self.value = @options.reg_code if !@options.reg_code.to_s.empty?
+      end
+
+      # Set registration options according to the value
+      def store
+        if valid_url?
+          @options.reg_code   = ""
+          @options.custom_url = value
+        else
+          @options.reg_code   = value
+          @options.custom_url = default_url
+        end
+      end
+
+      def registered?
+        Registration.is_registered?
+      end
+
+      def register
+        return true if skip?
+
+        log.info("Registering the system and the base product.")
+        return false if !register_system_and_base_product
+
+        log.info("System registered with success.")
+        Storage::InstallationOptions.instance.base_registered = true
+      end
+
+    private
+
+      def valid_url?
+        return false if value.to_s.empty?
+
+        uri = URI(value)
+        VALID_URL_SCHEMES.include?(uri.scheme)
+      end
+
+      # run the system and the base product registration
+      # @return [Boolean] true on success
+      def register_system_and_base_product
+        url = UrlHelpers.registration_url
+        return false if UrlHelpers.registration_url == :cancel
+
+        registration    = Registration.new(url)
+        registration_ui = RegistrationUI.new(registration)
+
+        success, product_service = registration_ui.register_system_and_base_product
+
+        if product_service && !registration_ui.install_updates?
+          registration_ui.disable_update_repos(product_service)
+        end
+
+        success
+      end
+
+      # Default registration server
+      #
+      # The boot_url takes precedence over the SUSE::Connect default
+      # one.
+      #
+      # @return [String] URL for the registration server
+      def default_url
+        boot_url || SUSE::Connect::Config.new.url
+      end
+
+      # Registration server URL given through Linuxrc
+      #
+      # @return [String,nil] URL for the registration server; nil if not given.
+      def boot_url
+        UrlHelpers.boot_reg_url
+      end
+
+      # Skip registration if the value is empty or nil
+      def skip?
+        value.to_s.empty?
+      end
     end
   end
 end
