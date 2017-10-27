92392980a84a3ceb52cc7fb67a115ea20f798a9c
Ladislav Slezák <lslezak@suse.cz>
2017-10-27 13:58:08 +0200

    Improve the ERB test

--- test/erb_renderer_spec.rb
+++ test/erb_renderer_spec.rb
@@ -3,9 +3,15 @@ require_relative "spec_helper"
 require "registration/erb_renderer.rb"
 
 describe Registration::ErbRenderer do
-  subject { Registration::ErbRenderer.new(::Registration::Storage::Config.instance) }
+  subject { Registration::ErbRenderer.new(Registration::Storage::Config.instance) }
 
-  it "renders an ERB file" do
+  it "renders the ERB file as String" do
     expect(subject.render_erb_template("autoyast_summary.erb")).to be_a(String)
+  end
+
+  it "renders the AY configuration" do
+    result = subject.render_erb_template("autoyast_summary.erb")
+    # test the header, it is always present regardless the actual AY configuration
+    expect(result).to include("Product Registration")
   end
 end
