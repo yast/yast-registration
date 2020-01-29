13e52366b86a5c8f1745d1f7e28f60875b8115e5
Imobach González Sosa <igonzalezsosa@suse.com>
2020-01-29 10:25:33 +0000

    Revert "Use Ruby &. operator"
    This reverts commit 8915a807c7e50b62402cb479edd6c5e8e09b5042.
    It does not play well with our RuboCop 0.41 configuration.

--- src/lib/registration/widgets/remote_packages_table.rb
+++ src/lib/registration/widgets/remote_packages_table.rb
@@ -78,7 +78,7 @@ module Registration
         [
           package_status(item),
           item.name,
-          item.addon&.name.to_s
+          item.addon ? item.addon.name : ""
         ]
       end
 
