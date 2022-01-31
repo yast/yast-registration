001455e0ec0b5bf4e10d7679969d73107a67d4e6
Imobach González Sosa <igonzalezsosa@suse.com>
2022-01-31 12:58:28 +0000

    Improve comment on bsc#1194440

--- src/lib/registration/clients/scc_auto.rb
+++ src/lib/registration/clients/scc_auto.rb
@@ -96,8 +96,9 @@ module Registration
         # available from other sources
         product = Yast::AutoinstFunctions.selected_product
         # If the real product (an instance from the Product class) is not
-        # available (e.g. Online medium), just skip reading the regcode because the short_name is
-        # unknown at this point. See bsc#1194440.
+        # available (e.g. Online medium), just skip reading the regcode because
+        # the short_name (which is required to find the regcode) is unknown at
+        # this point. See bsc#1194440.
         if product&.respond_to?(:short_name) && !settings["reg_code"]
           reg_codes_loader = ::Registration::Storage::RegCodes.instance
           settings["reg_code"] = reg_codes_loader.reg_codes[product.short_name] || ""
